008af9986ebfde05a706c1cc4df7dd62d6fec781
phanvu <pcnhatvu@gmail.com>
2022-09-19 21:12:24 +0700

    Initialize project

---
+++ src/main/java/com/myblog/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.myblog.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return new DriverManagerDataSource("jdbc:mysql://localhost:3306/personal_finance", "root", "123456");
+	}
+}
